0b5a7932a0ad2ae4a6a5c1df11f22e13ac81bb2c
Mr-FuzzyPenguin <scokro7141@gmail.com>
2022-07-21 09:40:45 -0400

    Updated July 20 script.js

--- July 20/script.js
+++ July 20/script.js
@@ -1,30 +1,52 @@
-let fish = {
-  "can_swim" : 1,
-  "can_fly"  : false,
-  "can_walk"  : false,
-  "noise" : "none"
-}
-let bird = {
-  "can_swim" : 0.5,
-  "can_fly"  : true,
-  "can_walk"  : true,
-  "noise" : "chirp"
-}
-let cat = {
-  "can_swim" : 0,
-  "can_fly"  : false,
-  "can_walk"  : true,
-  "noise" : "MEOW!"
-}
-let horse = {
-  "can_swim" : 0,
-  "can_fly"  : false,
-  "can_walk"  : true,
-  "noise" : "neighs"
+const scrambled_numbers = [
+  9, 25, 28, 22, 21, 0, 27, 14, 23, 26, 13, 12, 19, 20, 7, 29, 4, 6, 15, 17, 16,
+  24, 5, 3, 8, 1, 2, 11, 18, 10,
+];
+const scrambled_words = [
+  "in",
+  "and",
+  "their",
+  "away",
+  "mile",
+  "Before",
+  "have",
+  "when",
+  "from",
+  "you",
+  "way",
+  "That",
+  "are",
+  "a",
+  "a",
+  "shoes.",
+  "you",
+  "walk",
+  "you",
+  "them,",
+  "criticize",
+  "them",
+  "should",
+  "someone,",
+  "mile",
+  "you",
+  "criticize",
+  "shoes.",
+  "you",
+  "their"
+];
+
+let unscrambled_words = [];
+
+for (const number of scrambled_numbers)
+{
+	unscrambled_words.push(scrambled_words[number]);
 }
 
 
-let animals = [fish, bird, cat, horse]
 
-for (animal of animals)
-  {console.log(animal);}
+
+
+/* Your code - Note: save your unscrambled array of words as "unscrambled_words" */
+let secret_message = unscrambled_words.join(" ");
+
+console.log(secret_message);
