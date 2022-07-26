a2d61c517412c262268c03ecf40cef098f65d6ea
Mr-FuzzyPenguin <scokro7141@gmail.com>
2022-07-25 23:00:42 -0400

    Download JSON Headers. Still need to fix promise errors

--- July 25/script.js
+++ July 25/script.js
@@ -9,18 +9,8 @@ const scoreSpan = document.querySelector("#score");
 const questionDiv = document.querySelector("#question");
 
 const pointArray = [100, 200, 300, 400, 1000]
+const categoryArray = [17, 9, 19, 29, 499]
 
-// Select the buttons and input fields where users can provide inputs.
-const randomButton = document.querySelector("#random");
-const hardButton = document.querySelector("#hard");
-const catPunsButton = document.querySelector("#catPuns");
-const submitButton = document.querySelector("#submit");
-
-
-// Starting variables - we'll fill replace these with the API
-let currentQuestion = '';
-let currentAnswer = '';
-let currentPoints = 0;
 let currentScore = 0;
 
 
@@ -29,14 +19,14 @@ let temp_arr = [];
 const tablerow = document.querySelector("#table-row")
 for (let i=0; i<5; i++)
 {
-  temp_arr.push(tablerow.innerHTML)
+  temp_arr.push(tablerow.innerHTML.replace(`id=""`,`id="`+i+`-"`))
 }
 tablerow.innerHTML = temp_arr.join('')
 
 temp_arr = []
 for (let i=0; i<5; i++)
 {
-  temp_arr.push(tablerow.outerHTML.replaceAll("[X00]",pointArray[i]))
+  temp_arr.push(tablerow.outerHTML.replaceAll("[X00]",pointArray[i]).replaceAll("-\"",'-'+i+'""'))
 }
 tablerow.outerHTML = temp_arr.join('')
 
@@ -46,13 +36,47 @@ delete(temp_arr);
 
 
 
+
+question_array = []
+
+const getQuestion = async (value,category) => {
+  const response = await fetch("https://jeopardy.wang-lu.com/api/clues?value=" +value+"&category="+category);
+  const data = await response.json();
+  const i = getRandomInt(data.length);
+
+  if (response.ok)
+  {
+    return data[i];
+  }
+// For debugging purposes, because I don't want data that yields undefined
+//   if (data[i]===undefined)
+//   {
+//     console.log(value,category,"yields undefined")
+//   }
+};
+
+let received_questions = []
+for (let c=0; c<5; c++){
+  for (let i=0; i<5; i++)
+  {
+     received_questions.push(getQuestion(pointArray[i],categoryArray[c]));
+  }
+}
+
+for (i of received_questions)
+{
+  console.log(i)
+}
+
+
+
 // Styling and card-flipping
 // UX
 const cards = document.querySelectorAll(".flip-card")
 
 cards.forEach((a_card) => {
 
-
+  const submitButton = a_card.querySelector("#submit")
 
   // Finish this function that checks the user's answer.
   const checkAnswer = () => {
@@ -70,7 +94,6 @@ cards.forEach((a_card) => {
     {
       currentScore -= currentPoints
     }
-    updateBoard();
   };
 
 
